6bf069affe583fa926e0024155cfee4feeda6015
amaolls <amddeoliveira@gmail.com>
2026-03-26 20:43:49 -0300

    Create Questao_1.ts

---
+++ src/Questao_1.ts
@@ -0,0 +1,11 @@
+// Crie um programa que receba um número e informe se ele é positivo, negativo ou zero.
+
+let valor: number = Number(prompt("Informe um numero: "))
+
+if (valor > 0){
+    alert("Número Positivo!")
+} else if (valor < 0){
+    alert("Número Negativo!")
+} else{
+    alert("Número Zero!")
+}
