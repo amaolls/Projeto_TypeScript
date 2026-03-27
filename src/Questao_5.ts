3de88e22df675a8e968e82b88ee6b71a4bd03ce6
amaolls <amddeoliveira@gmail.com>
2026-03-26 21:18:04 -0300

    Update Questao_5.ts

--- src/Questao_5.ts
+++ src/Questao_5.ts
@@ -1,2 +1,17 @@
 // Crie uma função que receba a idade de uma pessoa e informe se ela é maior de idade.
 
+let idade: number = Number(prompt("Informe a Idade:"))
+
+function maior_idade(id:number){
+    if (id >= 18 && id <= 100){
+        alert("Idade: "+id+". Maior de Idade!")
+    } else if(id >= 0 && id < 18){
+        alert("Idade: "+id+". Menor de Idade!")
+    }else{
+        alert("Invalido! Tente novamente!")
+    }
+        
+    
+}
+
+maior_idade(idade)
