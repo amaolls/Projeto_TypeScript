fbb42ecdc7714bdaf0ff4a82653cb2e93292db92
amaolls <amddeoliveira@gmail.com>
2026-03-27 21:50:42 -0300

    Create Questao_8.ts

---
+++ src/Exercicios/Questao_8.ts
@@ -0,0 +1,10 @@
+// Use um laço for para somar todos os números de 1 a 100.
+export function Questao_8(): any {
+    let somar: number = 0
+
+for (let i: number = 1; i <= 100; i++){
+    somar += i
+    
+}
+alert(somar)
+}
