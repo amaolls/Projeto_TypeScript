da22002c0a0b0e9bdd6a0a3fd041c76f1d8fb355
amaolls <amddeoliveira@gmail.com>
2026-03-27 21:50:58 -0300

    Create Questao_4.ts

---
+++ src/Exercicios/Questao_4.ts
@@ -0,0 +1,10 @@
+// Use um laço while para imprimir os números de 10 a 1.
+export function Questao_4(): any {
+    let imprimir: number = 10
+
+    while (imprimir >= 1){
+        console.log(imprimir)
+        imprimir--
+    }
+}
+
