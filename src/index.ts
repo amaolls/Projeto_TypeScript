e1261031c19ecdfce32b6b4c78eeac015799f86a
amaolls <amddeoliveira@gmail.com>
2026-03-27 21:51:26 -0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Questao_1 } from "./Exercicios/Questao_1"
+import { Questao_01 } from "./Exercicios/Questao_01"
+import { Questao_3 } from "./Exercicios/Questao_3"
+import { Questao_4 } from "./Exercicios/Questao_4"
+import { Questao_5 } from "./Exercicios/Questao_5"
+import { Questao_6 } from "./Exercicios/Questao_6"
+import { Questao_7 } from "./Exercicios/Questao_7"
+import { Questao_8 } from "./Exercicios/Questao_8"
+
+function executarMenu(opcao: number) {
+    console.clear()
+    console.log(`Opção selecionada: ${opcao}`)
+
+    switch (opcao) {
+        case 1: Questao_1(); break
+        case 2: Questao_01(); break
+        case 3: Questao_3(); break
+        case 4: Questao_4(); break
+        case 5: Questao_5(); break
+        case 6: Questao_6(); break
+        case 7: Questao_7(); break
+        case 8: Questao_8(); break
+        default: console.log("Opção inválida"); break
+    }
+}
+
+document.getElementById('btn-q1')?.addEventListener('click', () => executarMenu(1));
+document.getElementById('btn-q2')?.addEventListener('click', () => executarMenu(2));
+document.getElementById('btn-q3')?.addEventListener('click', () => executarMenu(3));
+document.getElementById('btn-q4')?.addEventListener('click', () => executarMenu(4));
+document.getElementById('btn-q5')?.addEventListener('click', () => executarMenu(5));
+document.getElementById('btn-q6')?.addEventListener('click', () => executarMenu(6));
+document.getElementById('btn-q7')?.addEventListener('click', () => executarMenu(7));
+document.getElementById('btn-q8')?.addEventListener('click', () => executarMenu(8));
+
+
