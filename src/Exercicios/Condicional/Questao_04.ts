c3019163658cc24ab17315fa00693bda4dd3099b
amaolls <amddeoliveira@gmail.com>
2026-04-10 21:09:27 -0300

    Create Questao_04.ts

---
+++ src/Exercicios/Condicional/Questao_04.ts
@@ -0,0 +1,4 @@
+// Ler dois valores e imprimir uma das três mensagens a seguir:
+// ● ‘Números iguais’, caso os números sejam iguais;
+// ● ‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
+// ● ‘Segundo maior’, caso o segundo seja maior que o primeiro.
