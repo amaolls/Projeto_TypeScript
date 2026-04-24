f98613c26fa02bb0cda9dd3c238b8191ecf72fb8
amaolls <amddeoliveira@gmail.com>
2026-04-24 20:14:54 -0300

    Create Questao_9.ts

---
+++ src/Exercicios/Array/Questao_9.ts
@@ -0,0 +1,5 @@
+// Crie uma função que recebe dois números: início e fim. Depois use um laço para preencher um
+// array com o intervalo e imprimir apenas os números que são múltiplos de 3.
+export function Questao_9(){
+
+}
