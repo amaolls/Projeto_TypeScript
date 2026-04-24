cec1f28507c5d387386e0e25a6336feb93b25355
amaolls <amddeoliveira@gmail.com>
2026-04-24 20:15:00 -0300

    Create Questao_001.ts

---
+++ src/Exercicios/Funcoes/Questao_001.ts
@@ -0,0 +1,8 @@
+// Crie uma função somarAte que recebe um número inteiro positivo. Use um laço de repetição
+// para calcular a soma de todos os números de 1 até o número fornecido. (Ex: somarAte(4)
+// deve retornar 10, pois 1+2+3+4 = 10).
+
+
+export function Questao_001(){
+    
+}
