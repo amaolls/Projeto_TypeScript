82bfeeefc7d4f3ec93e96662ea56424ce328985e
amaolls <amddeoliveira@gmail.com>
2026-04-24 20:14:27 -0300

    Create Questao_3.ts

---
+++ src/Exercicios/Array/Questao_3.ts
@@ -0,0 +1,17 @@
+// Gerador de Lista de Compras Personalizada
+// Sua mãe pediu para você fazer uma lista de compras para o supermercado. Ela quer que você possa
+// adicionar itens e a quantidade de cada um.
+// Crie uma função chamada gerar_lista_compras() que não recebe argumentos. A função deve:
+// ● Permitir que o usuário adicione itens à lista(array) até que ele digite &quot;fim&quot;.
+// ● Permitir que o usuário apresente todos os itens da lista.
+// ● Permitir que o usuário apresente quantos itens há na lista.
+// ● Permitir que o usuário remova itens da lista.
+export function Questao_3(): any {
+    let imprimir: number = 10
+
+    while (imprimir >= 1){
+        console.log(imprimir)
+        imprimir--
+    }
+}
+
