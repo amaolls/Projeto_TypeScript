fe3db6255596db3dbdebce2f1b2af8f70734bcbb
amaolls <amddeoliveira@gmail.com>
2026-04-24 20:14:37 -0300

    Create Questao_5.ts

---
+++ src/Exercicios/Array/Questao_5.ts
@@ -0,0 +1,26 @@
+// Organizador de Tarefas Diárias
+// Você quer organizar suas tarefas de casa e da escola para não esquecer nada. Crie uma função
+// chamada gerenciar_tarefas() que não receba argumentos. A função deve:
+// a) Permitir que o usuário adicione tarefas a um vetor.
+// b) Permitir que o usuário marque tarefas como concluídas (removendo-as da lista, por
+// exemplo).
+// c) Permitir que o usuário exiba todas as tarefas pendentes.
+// Utilize um menu interativo com opções (adicionar, concluir, exibir, sair) e um laço while para
+// manter o programa rodando até o usuário escolher sair.
+
+export function Questao_5(): any {
+     
+ let contador: number = 0 
+
+for (let i: number = 1; i <= 5; i++){
+    let num : number = Number(prompt("Digite 5 valores: "))
+    
+    if (num > 0){
+        contador++
+    }
+    console.log("Números Positivos: "+contador)
+    console.log("Números digitados: "+num)
+}
+}
+
+
