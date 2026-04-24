d37807dbef20d41e436f96b80ab6736c99700010
amaolls <amddeoliveira@gmail.com>
2026-04-24 20:16:49 -0300

    Update Questao_04.ts

--- src/Exercicios/Condicional/Questao_04.ts
+++ src/Exercicios/Condicional/Questao_04.ts
@@ -2,3 +2,16 @@
 // ● ‘Números iguais’, caso os números sejam iguais;
 // ● ‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
 // ● ‘Segundo maior’, caso o segundo seja maior que o primeiro.
+
+export function Questao_04(): void {
+    let num1: number = Number(prompt("Digite o primeiro número: "))
+    let num2: number = Number(prompt("Digite o segundo número: "))
+
+    if (num1 === num2) {
+        alert("Números iguais")
+    } else if (num1 > num2) {
+        alert("Primeiro é maior")
+    } else {
+        alert("Segundo maior")
+    }
+}
