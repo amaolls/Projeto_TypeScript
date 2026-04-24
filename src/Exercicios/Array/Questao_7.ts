63a7544c0fb803c6fcf98e1adb69211952bfd5f7
amaolls <amddeoliveira@gmail.com>
2026-04-24 20:14:45 -0300

    Create Questao_7.ts

---
+++ src/Exercicios/Array/Questao_7.ts
@@ -0,0 +1,17 @@
+// Lista de Presença
+
+// 1. Crie um array chamado presenca que aceite apenas strings.
+// 2. Adicione 5 nomes de alunos.
+// 3. Tente adicionar um número e observe o erro do compilador.
+// 4. Use um loop para imprimir cada nome em letras maiúsculas.
+
+
+export function Questao_7(): any {
+    let somar: number = 0
+
+for (let i: number = 1; i <= 100; i++){
+    somar += i
+    
+}
+alert(somar)
+}
