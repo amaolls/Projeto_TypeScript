81b871834bc4070cce5290eb497e39e566532aba
amaolls <amddeoliveira@gmail.com>
2026-04-24 21:22:53 -0300

    Create fun_6.ts

---
+++ src/Exercicios/Funcoes/fun_6.ts
@@ -0,0 +1,19 @@
+// Faça um programa para o cálculo de uma folha de pagamento fictício, sabendo que os
+// descontos são do Imposto de Renda, que depende do salário bruto (conforme tabela abaixo),
+// 3% para o Sindicato, 6% de vale transporte, 8% de vale alimentação, 10% do INSS e o FGTS
+// que corresponde a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita).
+
+// O Salário Líquido corresponde ao Salário Bruto menos os descontos. O programa deverá
+// pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+// Desconto do IR:
+// 1. Salário Bruto até R$ 2428,80 (inclusive) - isento
+// 2. Salário Bruto de R$ 2.428,81 a R$ 2.826,65 (inclusive) - desconto de 7,5%
+// 3. Salário Bruto de R$ 2.826,66 a R$ 3.751,05 (inclusive) - desconto de 15%
+// 4. Salário Bruto de R$ 3.751,06 a R$ 4.664,68 (inclusive) - desconto de 22,5%
+// 5. Salário Bruto acima de R$4664,68 - desconto de 27,5%.
+// Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora
+// é R$15,00 e a quantidade de horas é 220.
+
+export function fun_6(){
+    
+}
