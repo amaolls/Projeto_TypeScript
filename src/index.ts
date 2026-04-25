3028bd556391636e22c77fe084445ca442b3854b
amaolls <amddeoliveira@gmail.com>
2026-04-24 21:23:19 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import { Questao_01 } from "./Exercicios/Condicional/Questao_01.js"
-import { Questao_02 } from "./Exercicios/Condicional/Questao_02.js"
-import { Questao_03 } from "./Exercicios/Condicional/Questao_03.js"
-import { Questao_04 } from "./Exercicios/Condicional/Questao_04.js"
+import { cond_1 } from "./Exercicios/Condicional/cond_1.js"
+import { cond_2 } from "./Exercicios/Condicional/cond_2.js"
+import { cond_3 } from "./Exercicios/Condicional/cond_3.js"
+import { cond_4 } from "./Exercicios/Condicional/cond_4.js"
 //
 
-import { Questao_001 } from "./Exercicios/Funcoes/Questao_001.js"
-import { Questao_002 } from "./Exercicios/Funcoes/Questao_002.js"
-import { Questao_003 } from "./Exercicios/Funcoes/Questao_003.js"
-import { Questao_004 } from "./Exercicios/Funcoes/Questao_004.js"
-import { Questao_05 } from "./Exercicios/Funcoes/Questao_05.js"
-import { Questao_06 } from "./Exercicios/Funcoes/Questao_06.js"
-import { Questao_07 } from "./Exercicios/Funcoes/Questao_07.js"
-import { Questao_08 } from "./Exercicios/Funcoes/Questao_08.js"
-import { Questao_09 } from "./Exercicios/Funcoes/Questao_09.js"
-import { Questao_10 } from "./Exercicios/Funcoes/Questao_10.js"
+import { fun_1 } from "./Exercicios/Funcoes/fun_1.js"
+import { fun_2 } from "./Exercicios/Funcoes/fun_2.js"
+import { fun_3 } from "./Exercicios/Funcoes/fun_3.js"
+import { fun_4 } from "./Exercicios/Funcoes/fun_4.js"
+import { fun_5 } from "./Exercicios/Funcoes/fun_5.js"
+import { fun_6 } from "./Exercicios/Funcoes/fun_6.js"
+import { fun_7 } from "./Exercicios/Funcoes/fun_7.js"
+import { fun_8 } from "./Exercicios/Funcoes/fun_8.js"
+import { fun_9 } from "./Exercicios/Funcoes/fun_9.js"
+import { fun_10 } from "./Exercicios/Funcoes/fun_10.js"
 //
-import { Questao_0001 } from "./Exercicios/Repeticao/Questao_0001.js"
-import { Questao_0002 } from "./Exercicios/Repeticao/Questao_0002.js"
-import { Questao_0003 } from "./Exercicios/Repeticao/Questao_0003.js"
-import { Questao_0004 } from "./Exercicios/Repeticao/Questao_0004.js"
-import { Questao_005 } from "./Exercicios/Repeticao/Questao_005.js"
+import { rep_1 } from "./Exercicios/Repeticao/rep_1.js"
+import { rep_2 } from "./Exercicios/Repeticao/rep_2.js"
+import { rep_3 } from "./Exercicios/Repeticao/rep_3.js"
+import { rep_4 } from "./Exercicios/Repeticao/rep_4.js"
+import { rep_5 } from "./Exercicios/Repeticao/rep_5.js"
 //
 import { Questao_1 } from "./Exercicios/Array/Questao_1.js"
 import { Questao_2 } from "./Exercicios/Array/Questao_2.js"
@@ -35,27 +35,27 @@ import { Questao_9 } from "./Exercicios/Array/Questao_9.js"
 
 
 
-document.getElementById('btn-q1')?.addEventListener('click', () => Questao_01)
-document.getElementById('btn-q2')?.addEventListener('click', () => Questao_02)
-document.getElementById('btn-q3')?.addEventListener('click', () => Questao_03)
-document.getElementById('btn-q4')?.addEventListener('click', () => Questao_04)
+document.getElementById('btn-q1')?.addEventListener('click', () => cond_1)
+document.getElementById('btn-q2')?.addEventListener('click', () => cond_2)
+document.getElementById('btn-q3')?.addEventListener('click', () => cond_3)
+document.getElementById('btn-q4')?.addEventListener('click', () => cond_4)
 //
-document.getElementById('btn-q5')?.addEventListener('click', () => Questao_001)
-document.getElementById('btn-q6')?.addEventListener('click', () => Questao_002)
-document.getElementById('btn-q7')?.addEventListener('click', () => Questao_003)
-document.getElementById('btn-q8')?.addEventListener('click', () => Questao_004)
-document.getElementById('btn-q9')?.addEventListener('click', () => Questao_05)
-document.getElementById('btn-q10')?.addEventListener('click', () => Questao_06)
-document.getElementById('btn-q11')?.addEventListener('click', () => Questao_07)
-document.getElementById('btn-q12')?.addEventListener('click', () => Questao_08)
-document.getElementById('btn-q13')?.addEventListener('click', () => Questao_09)
-document.getElementById('btn-q14')?.addEventListener('click', () => Questao_10)
+document.getElementById('btn-q5')?.addEventListener('click', () => fun_1)
+document.getElementById('btn-q6')?.addEventListener('click', () => fun_2)
+document.getElementById('btn-q7')?.addEventListener('click', () => fun_3)
+document.getElementById('btn-q8')?.addEventListener('click', () => fun_4)
+document.getElementById('btn-q9')?.addEventListener('click', () => fun_5)
+document.getElementById('btn-q10')?.addEventListener('click', () => fun_6)
+document.getElementById('btn-q11')?.addEventListener('click', () => fun_7)
+document.getElementById('btn-q12')?.addEventListener('click', () => fun_8)
+document.getElementById('btn-q13')?.addEventListener('click', () => fun_9)
+document.getElementById('btn-q14')?.addEventListener('click', () => fun_10)
 //
-document.getElementById('btn-q15')?.addEventListener('click', () => Questao_0001)
-document.getElementById('btn-q16')?.addEventListener('click', () => Questao_0002)
-document.getElementById('btn-q17')?.addEventListener('click', () => Questao_0003)
-document.getElementById('btn-q18')?.addEventListener('click', () => Questao_0004)
-document.getElementById('btn-q19')?.addEventListener('click', () => Questao_005)
+document.getElementById('btn-q15')?.addEventListener('click', () => rep_1)
+document.getElementById('btn-q16')?.addEventListener('click', () => rep_2)
+document.getElementById('btn-q17')?.addEventListener('click', () => rep_3)
+document.getElementById('btn-q18')?.addEventListener('click', () => rep_4)
+document.getElementById('btn-q19')?.addEventListener('click', () => rep_5)
 //
 document.getElementById('btn-q20')?.addEventListener('click', () => Questao_1)
 document.getElementById('btn-q21')?.addEventListener('click', () => Questao_2)
