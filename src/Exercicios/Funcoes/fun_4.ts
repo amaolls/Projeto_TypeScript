ffad0711e884f607b8ed77d3d9e03dbaf074c33c
amaolls <amddeoliveira@gmail.com>
2026-04-28 20:43:00 -0300

    Update fun_4.ts

--- src/Exercicios/Funcoes/fun_4.ts
+++ src/Exercicios/Funcoes/fun_4.ts
@@ -2,6 +2,40 @@
 // de 50, 20 e 10 são necessárias para o saque (priorizando as maiores). Use um laço while
 // para ir subtraindo do valor total.
 
-export function fun_4(){
+export function fun_4(): void {
     
+    alert("Caixa Eletrônico - Notas de 50, 20 e 10")
+    
+    let valor: number = Number(prompt("Digite o valor do saque: "))
+    
+    function calcularNotas(saque: number): void {
+        let notas50: number = 0
+        let notas20: number = 0
+        let notas10: number = 0
+        let restante: number = saque
+        
+        while (restante >= 50) {
+            notas50++;
+            restante -= 50
+        }
+        
+        while (restante >= 20) {
+            notas20++;
+            restante -= 20
+        }
+        
+        while (restante >= 10) {
+            notas10++;
+            restante -= 10
+        }
+        
+        alert(
+            "Valor do saque: " + saque +
+            "\nNotas de 50: " + notas50 +
+            "\nNotas de 20: " + notas20 +
+            "\nNotas de 10: " + notas10
+        );
+    }
+    
+    calcularNotas(valor)
 }
