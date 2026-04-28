089bd8854e9b014572d6a8aa167ad95d4ce94ebd
amaolls <amddeoliveira@gmail.com>
2026-04-28 20:43:04 -0300

    Update fun_3.ts

--- src/Exercicios/Funcoes/fun_3.ts
+++ src/Exercicios/Funcoes/fun_3.ts
@@ -5,6 +5,21 @@
 // **
 // ***
 
-export function fun_3(){
+export function fun_3(): void {
     
+    alert("Triângulo de asteriscos no console")
+    
+    let num: number = Number(prompt("Digite um número: "))
+    
+    function triangulo(n: number): void {
+        for (let i: number = 1; i <= n; i++) {
+            let linha: string = "";
+            for (let j: number = 1; j <= i; j++) {
+                linha += "*"
+            }
+            console.log(linha)
+        }
+    }
+    
+    triangulo(num)
 }
