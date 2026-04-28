fd6782fe45cc306ccb6958d703aa2e27373ce32d
amaolls <amddeoliveira@gmail.com>
2026-04-28 20:43:12 -0300

    Update fun_1.ts

--- src/Exercicios/Funcoes/fun_1.ts
+++ src/Exercicios/Funcoes/fun_1.ts
@@ -3,17 +3,20 @@
 // deve retornar 10, pois 1+2+3+4 = 10).
 
 
-export function fun_1(){
-   
-   let num_info:number = Number(prompt("Esse programa calcula a soma de todos os números de 1 até o número fornecido. \n Informe um número: "))
-    function somarAte(n:number):number{
-        let soma:number = 0
+export function fun_1(): void {
+    
+    alert("Esse programa calcula a soma de todos os números de 1 até o número fornecido.")
+    let num_info: number = Number(prompt("Informe um número: "))
+    
+    function somarAte(n: number): number {
+        let soma: number = 0
 
-        for (let i: number = 1; i <= n; i++){
+        for (let i: number = 1; i <= n; i++) {
             soma += i
         }
         return soma
     }
-somarAte(num_info)
-alert("A soma dos elemento dos elementos contidos no número "+num_info+" é"+somarAte)
+    
+    let resultado: number = somarAte(num_info)
+    alert(`A soma dos elementos contidos no número ${num_info} é ${resultado}`)
 }
