45170b1cd054dc7d78756a23dba76b3f672bd1b8
amaolls <amddeoliveira@gmail.com>
2026-04-29 17:21:20 -0300

    Update rep_2.ts

--- src/Exercicios/Repeticao/rep_2.ts
+++ src/Exercicios/Repeticao/rep_2.ts
@@ -1,6 +1,15 @@
 // Desenvolva a tabuada de um número usando for.
 
 
-export function rep_2(){
-    
+export function rep_2(): void {
+
+    alert("Tabuada do número")
+    let num: number = Number(prompt("Digite um número para ver sua tabuada: "))
+    let tabuada: string = ""
+
+    for (let i: number = 1; i <= 10; i++) {
+        tabuada += `${num} x ${i} = ${num * i}\n`
+    }
+
+    alert(tabuada)
 }
