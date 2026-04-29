539799a0b4fb489f82b5dab7ba232295b1a67fcf
amaolls <amddeoliveira@gmail.com>
2026-04-29 17:22:25 -0300

    Update Questao_7.ts

--- src/Exercicios/Array/Questao_7.ts
+++ src/Exercicios/Array/Questao_7.ts
@@ -6,12 +6,27 @@
 // 4. Use um loop para imprimir cada nome em letras maiúsculas.
 
 
-export function Questao_7(): any {
-    let somar: number = 0
+export function Questao_7(): void {
+  
+    alert("Lista de Presença")
+    let presenca: string[] = []
 
-for (let i: number = 1; i <= 100; i++){
-    somar += i
-    
-}
-alert(somar)
+   
+    for (let i: number = 1; i <= 5; i++) {
+        let nome: string | null = prompt("Digite o nome do " + i + "º aluno:")
+        if (nome !== null) {
+            presenca.push(nome)
+        } else {
+            presenca.push("")
+        }
+    }
+
+    alert("Lista de presença adicionada com sucesso!")
+
+
+    let listaMaiuscula: string = "Lista de presença (maiúsculas):\n"
+    for (let i: number = 0; i < presenca.length; i++) {
+        listaMaiuscula += presenca[i].toUpperCase() + "\n"
+    }
+    alert(listaMaiuscula)
 }
