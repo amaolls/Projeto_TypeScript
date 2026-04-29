1f4fcfdf788c49742625e16b87aed3e4e0108666
amaolls <amddeoliveira@gmail.com>
2026-04-29 17:21:27 -0300

    Update rep_4.ts

--- src/Exercicios/Repeticao/rep_4.ts
+++ src/Exercicios/Repeticao/rep_4.ts
@@ -2,6 +2,21 @@
 // números pares (QPares) e a quantidade de números ímpares (QImpares) lidos. Admita que o
 // valor -1 é utilizado como sentinela para fim de leitura.
 
-export function rep_4(){
-    
+export function rep_4(): void {
+    alert("Contador de pares e ímpares (-1 para sair)")
+
+    let QPares: number = 0
+    let QImpares: number = 0
+    let X: number = Number(prompt("Digite um número (-1 para encerrar): "))
+
+    while (X !== -1) {
+        if (X % 2 === 0) {
+            QPares++
+        } else {
+            QImpares++
+        }
+        X = Number(prompt("Digite outro número (-1 para encerrar): "))
+    }
+
+    alert("Quantidade de pares: " + QPares + "\nQuantidade de ímpares: " + QImpares)
 }
