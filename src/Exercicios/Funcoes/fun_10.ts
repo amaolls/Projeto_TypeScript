74dafc892918cfe0c5963669ad8f7fae7db9910f
amaolls <amddeoliveira@gmail.com>
2026-04-29 17:21:52 -0300

    Update fun_10.ts

--- src/Exercicios/Funcoes/fun_10.ts
+++ src/Exercicios/Funcoes/fun_10.ts
@@ -9,6 +9,37 @@
 // [classificacao]&quot;.
 //  Uso: Peça o nome, peso e altura, processe os dados e exiba o laudo.
 
-export function fun_10(){
-    
+export function fun_10(): void {
+
+    alert("Calculadora de IMC com Classificação")
+
+    let nome_cliente: string = String (prompt("Informe o nome do paciente: "))
+    let peso: number = Number(prompt("Informe o peso (kg): "))
+    let altura: number = Number(prompt("Informe a altura (m): "))
+
+
+    function calcular_imc(peso: number, altura: number): number {
+        let imc: number = peso / (altura * altura)
+        return imc
+    }
+
+    function classificar_imc(valor_imc: number): string {
+        if (valor_imc < 18.5) {
+            return "Abaixo do peso"
+        } else if (valor_imc >= 18.5 && valor_imc < 25) {
+            return "Peso Normal"
+        } else {
+            return "Sobrepeso"
+        }
+    }
+
+
+    function exibir_laudo(nome_cliente: string, classificacao: string): void {
+        alert(`Paciente: ${nome_cliente} \n Status: ${classificacao}`)
+    }
+
+
+    let imc: number = calcular_imc(peso, altura)
+    let classificacao: string = classificar_imc(imc)
+    exibir_laudo(nome_cliente, classificacao)
 }
