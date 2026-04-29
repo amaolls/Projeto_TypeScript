b7ba08b54aa938832e1b739e842f570bd4570447
amaolls <amddeoliveira@gmail.com>
2026-04-29 17:22:28 -0300

    Update Questao_8.ts

--- src/Exercicios/Array/Questao_8.ts
+++ src/Exercicios/Array/Questao_8.ts
@@ -6,6 +6,27 @@
 //  Se for um número, imprima: &quot;Nota: [valor]&quot;.
 //  Se for &quot;N.A&quot;, imprima: &quot;O aluno não realizou a prova&quot;.
 
-export function Questao_8(){
-    
+export function Questao_8(): void {
+    alert("Boletim Híbrido")
+
+
+    let notasSemestre: (number | string)[] = []
+
+
+    notasSemestre.push(8.5)
+    notasSemestre.push(10)
+    notasSemestre.push("N.A")
+    notasSemestre.push(7.2)
+
+    let resultado: string = ""
+    for (let i: number = 0; i < notasSemestre.length; i++) {
+        let elemento: number | string = notasSemestre[i]
+        if (elemento === "N.A") {
+            resultado += "O aluno não realizou a prova\n"
+        } else {
+            resultado += "Nota: " + elemento + "\n"
+        }
+    }
+
+    alert(resultado)
 }
