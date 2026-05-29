60f62b5c798208374aaa3926c9da6d489b358821
amaolls <amddeoliveira@gmail.com>
2026-05-29 20:03:33 -0300

    Create questao_11.ts

---
+++ src/Exercicios/POO/questao_11.ts
@@ -0,0 +1,2 @@
+// Uma lanchonete quer registrar pedidos dos clientes. O sistema deve solicitar o nome do cliente, o
+// nome do pedido e o valor. Crie um método que exiba o resumo do pedido e o valor total.
