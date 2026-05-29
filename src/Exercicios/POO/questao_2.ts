42b1ae064b55d53388b2c3f00cbca5a472a6747b
amaolls <amddeoliveira@gmail.com>
2026-05-29 20:03:14 -0300

    Create questao_2.ts

---
+++ src/Exercicios/POO/questao_2.ts
@@ -0,0 +1,10 @@
+// Classe Quadrado: Crie uma classe que modele um quadrado:
+//  Atributos: Tamanho do lado
+//  Métodos: Mudar valor do Lado,
+//  Retornar valor do Lado e calcular Área;
+
+
+class Quadrado{
+    private tamanho: number
+    private 
+}
