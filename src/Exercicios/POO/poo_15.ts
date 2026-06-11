037717618c32950cd1b94047129261e38c64bfd4
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:50 -0300

    Create poo_15.ts

---
+++ src/Exercicios/POO/poo_15.ts
@@ -0,0 +1,7 @@
+// Herança Encapsulamento
+// Uma empresa possui dois tipos de funcionários: horistas (pagos por hora trabalhada) e assalariados
+// (salário fixo mensal). Crie uma hierarquia de classes com Funcionario como superclasse e
+// FuncionarioHorista e FuncionarioAssalariado como subclasses. O programa deve solicitar os dados via
+// teclado e calcular o salário de cada um.
+
+export function poo_15 (){}
