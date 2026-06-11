50357648eeaf45a7fd359f159f86cba1ab00ca3d
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:12 -0300

    Delete questao_14.ts

--- src/Exercicios/POO/questao_14.ts
+++
@@ -1,4 +0,0 @@
-// Arrays Repetição Encapsulamento
-// Uma biblioteca precisa catalogar seus livros. Crie uma classe Livro com título, autor, ano de
-// publicação e disponibilidade (boolean). O programa deve permitir cadastrar até 15 livros via teclado,
-// listar todos os disponíveis e registrar o empréstimo de um livro pesquisado pelo título.
