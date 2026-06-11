d86fe2dbd5ce5d6077d72ee3c606e27a190d7b2a
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:57 -0300

    Create poo_12.ts

---
+++ src/Exercicios/POO/poo_12.ts
@@ -0,0 +1,6 @@
+// Uma locadora quer controlar os carros disponíveis. O sistema deve solicitar o modelo do carro, o
+// valor da diária e a quantidade de dias que o cliente deseja alugar. Crie um método que calcule o valor
+// total do aluguel e exiba o resumo da locação.
+
+
+export function poo_12 (){}
