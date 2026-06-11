b9d4b5c946b988175a2d39ddf5d65c376926a2e7
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:41:52 -0300

    Delete questao_7.ts

--- src/Exercicios/POO/questao_7.ts
+++
@@ -1,3 +0,0 @@
-// Uma empresa quer cadastrar funcionários e aplicar aumento salarial. O sistema deve pedir nome,
-// cargo e salário. Crie um método que receba um percentual de aumento e atualize o salário do
-// funcionário, exibindo o seu nome e novo valor.
