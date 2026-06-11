a421b2cbafbe4f910d9163ea3facffb0b064f3da
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:43:00 -0300

    Create poo_11.ts

---
+++ src/Exercicios/POO/poo_11.ts
@@ -0,0 +1,5 @@
+// Uma lanchonete quer registrar pedidos dos clientes. O sistema deve solicitar o nome do cliente, o
+// nome do pedido e o valor. Crie um método que exiba o resumo do pedido e o valor total.
+
+
+export function poo_11 (){}
