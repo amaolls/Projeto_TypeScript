2ecac24a7b034f8bb53cff42af7252dfa18cdfa7
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:52 -0300

    Create poo_14.ts

---
+++ src/Exercicios/POO/poo_14.ts
@@ -0,0 +1,6 @@
+// Arrays Repetição Encapsulamento
+// Uma biblioteca precisa catalogar seus livros. Crie uma classe Livro com título, autor, ano de
+// publicação e disponibilidade (boolean). O programa deve permitir cadastrar até 15 livros via teclado,
+// listar todos os disponíveis e registrar o empréstimo de um livro pesquisado pelo título.
+
+export function poo_14 (){}
