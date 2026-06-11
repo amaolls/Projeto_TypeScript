184f7da980f5a1f94ba191592ca05a8231fc97e5
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:32 -0300

    Create poo_7.ts

---
+++ src/Exercicios/POO/poo_7.ts
@@ -0,0 +1,5 @@
+// Uma empresa quer cadastrar funcionários e aplicar aumento salarial. O sistema deve pedir nome,
+// cargo e salário. Crie um método que receba um percentual de aumento e atualize o salário do
+// funcionário, exibindo o seu nome e novo valor.
+
+export function poo_7 (){}
