6361ccb0a0684ae74f6f64373d8ad06b17a98079
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:09 -0300

    Delete questao_15.ts

--- src/Exercicios/POO/questao_15.ts
+++
@@ -1,5 +0,0 @@
-// Herança Encapsulamento
-// Uma empresa possui dois tipos de funcionários: horistas (pagos por hora trabalhada) e assalariados
-// (salário fixo mensal). Crie uma hierarquia de classes com Funcionario como superclasse e
-// FuncionarioHorista e FuncionarioAssalariado como subclasses. O programa deve solicitar os dados via
-// teclado e calcular o salário de cada um.
