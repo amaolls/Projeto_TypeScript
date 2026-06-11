ff5a2648403816d2bd897d56639e6420ebbe09ce
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:41:57 -0300

    Delete questao_5.ts

--- src/Exercicios/POO/questao_5.ts
+++
@@ -1,5 +0,0 @@
-// Classe Pessoa: Crie uma classe que modele uma pessoa:
-// 1. Atributos: nome, idade, peso e altura
-// 2. Métodos: Envelhecer, engordar, emagrecer, crescer.
-// Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos,
-// ela deve crescer 0,5 cm.
