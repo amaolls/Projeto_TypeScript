34598055652ce434eef9bf3507fc0a7f1edcbcf4
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:43:07 -0300

    Create poo_1.ts

---
+++ src/Exercicios/POO/poo_1.ts
@@ -0,0 +1,37 @@
+// Classe Bola: Crie uma classe que modele uma bola:
+//  Atributos: Cor, circunferência, material
+//  Métodos: trocaCor e mostraCor
+
+export function poo_1 (){
+
+class Bola {
+
+     cor: string
+     circunferencia: number
+     material: string
+
+    constructor(co:string, ci:number, material:string){
+        this.cor = co
+        this.circunferencia = ci
+        this.material = material
+    }
+    trocarCor(novaCor:string): void{
+        this.cor = novaCor
+    }
+    mostraCor(): string{
+        return this.cor
+    }
+}
+
+for(let i=0; i<5; i++){
+
+    let co:string, ci: number, material: string
+    co = "Vermelha"
+    ci = 5
+    material = "Couro"
+
+    let bola_1:Bola = new Bola(co,ci,material)
+
+}
+
+}
