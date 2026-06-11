4b9f41bc647ea33336c9551a7badb48041342436
amaolls <amddeoliveira@gmail.com>
2026-06-11 19:42:07 -0300

    Delete questao_16.ts

--- src/Exercicios/POO/questao_16.ts
+++
@@ -1,5 +0,0 @@
-// Herança Polimorfismo Encapsulamento
-// Um zoológico possui mamíferos e aves. Ambos têm nome, espécie e idade. Mamíferos têm tipo de
-// alimentação; aves têm se são migratórias ou não. Cada animal tem um comportamento de &#39;emitir som&#39;
-// diferente. O sistema deve cadastrar animais, listar por tipo e simular a &#39;hora da alimentação&#39; chamando o
-// método de som de cada um.
