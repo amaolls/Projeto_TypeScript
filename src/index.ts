275316575da82ea4b2f879aef6da94573fadcc84
amaolls <amddeoliveira@gmail.com>
2026-06-11 20:46:46 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1212,7 +1212,36 @@ a lista criada.
 //POO
 
     //Q1
-    'btn-q30': ``,
+    'btn-q30': `// Classe Bola: Crie uma classe que modele uma bola:
+//  Atributos: Cor, circunferência, material
+//  Métodos: trocaCor e mostraCor
+
+export function poo_1 (){
+
+class Bola {
+
+     cor: string
+     circunferencia: number
+     material: string
+
+    constructor(cor:string, circunferencia:number, material:string){
+        this.cor = cor
+        this.circunferencia = circunferencia
+        this.material = material
+    }
+    trocarCor(novaCor:string): void{
+        this.cor = novaCor
+    }
+    mostraCor(): string{
+        return this.cor
+    }
+}
+
+
+
+    let bola_1:Bola = new Bola("vermelha",65,"couro")
+
+     alert('A bola tem as características:\nCor: $ {bola_1.mostraCor()}\nCircunferência: $ {bola_1.circunferencia}cm\nMaterial: $ {bola_1.material}')`,
     //Q2
     'btn-q31': ``,
     //Q3
