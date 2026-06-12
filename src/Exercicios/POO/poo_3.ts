77b04776323cecde6cae25d4d48677037528b0c7
amaolls <amddeoliveira@gmail.com>
2026-06-11 21:16:43 -0300

    Update poo_3.ts

--- src/Exercicios/POO/poo_3.ts
+++ src/Exercicios/POO/poo_3.ts
@@ -5,6 +5,44 @@
 //  Calcular Área,
 //  Calcular Perímetro.
 
-export function poo_3 (){
+export function poo_3(){
 
+    class Retangulo {
+        base: number
+        altura: number
+
+        constructor(base: number, altura: number) {
+            this.base = base
+            this.altura = altura
+        }
+
+        mudar_lados(nova_base: number, nova_altura: number) {
+            this.base = nova_base
+            this.altura = nova_altura
+        }
+
+        retornar_lados() {
+            return { base: this.base, altura: this.altura }
+        }
+
+        calcular_area() {
+            return this.base * this.altura
+        }
+
+        calcular_perimetro() {
+            return 2 * (this.base + this.altura)
+        }
+    }
+
+    let retangulo = new Retangulo(10, 5)
+    let lados = retangulo.retornar_lados()
+
+
+    alert(
+        `Características do Retângulo:\n` +
+        `- Base: ${lados.base}\n` +
+        `- Altura: ${lados.altura}\n` +
+        `- Área: ${retangulo.calcular_area()}\n` +
+        `- Perímetro: ${retangulo.calcular_perimetro()}`
+    )
 }
