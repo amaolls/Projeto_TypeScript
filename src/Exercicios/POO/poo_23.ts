ad1b9de106f2c1c7f6cad1fd90717ae5ae7b1fec
amaolls <amddeoliveira@gmail.com>
2026-07-03 20:39:12 -0300

    Create poo_23.ts

---
+++ src/Exercicios/POO/poo_23.ts
@@ -0,0 +1,13 @@
+// Cadastro de Produtos de um Supermercado com Desconto Progressivo
+// Um mercado de atacado precisa atualizar os preços de suas mercadorias nas prateleiras. Todo produto
+// possui código, nome e preço de custo ocultados do acesso externo direto. Os Produtos Perecíveis
+// possuem uma data de validade e recebem um desconto de 30% caso estejam no dia do vencimento. Os
+// Produtos Não Perecíveis não sofrem alteração de valor. O sistema deve interagir com o gerente para
+// listar os produtos do estoque. Após preencher o estoque (array), o programa deve rodar um loop que
+// simula a passagem do caixa, aplicando as regras de desconto conforme o tipo do produto e exibindo o
+// valor final que o cliente pagará.
+
+
+
+
+export function poo_23 (){}
