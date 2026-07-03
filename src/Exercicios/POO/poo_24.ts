f5502549a4df1d75a6388003d7a44fbd61d6720e
amaolls <amddeoliveira@gmail.com>
2026-07-03 20:39:10 -0300

    Create poo_24.ts

---
+++ src/Exercicios/POO/poo_24.ts
@@ -0,0 +1,12 @@
+// Gerenciador de Tarefas e Produtividade Acadêmica
+// Para ajudar os alunos a não perderem prazos, monte um gerenciador de tarefas. Uma tarefa genérica
+// possui uma descrição e o status de concluída (booleano). Uma Tarefa Acadêmica inclui o nome da
+// disciplina associada, enquanto uma Tarefa Pessoal inclui o nível de prioridade. O programa deve abrir
+// um menu para o estudante inserir suas tarefas diárias. O sistema armazena tudo em um array
+// unificado. Através da interação, o usuário pode escolher marcar uma tarefa como concluída ou listar
+// apenas as tarefas acadêmicas pendentes, utilizando a lógica de filtragem de propriedades dos objetos
+// contidos na lista.
+
+
+
+export function poo_24 (){}
