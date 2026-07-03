204dfb820591f44b4239e7bec7bec0abd1b3e593
amaolls <amddeoliveira@gmail.com>
2026-07-03 20:39:22 -0300

    Create poo_18.ts

---
+++ src/Exercicios/POO/poo_18.ts
@@ -0,0 +1,11 @@
+// Folha de Pagamento Unificada do IFS
+// O setor de Recursos Humanos necessita de uma ferramenta para calcular a folha salarial dos
+// colaboradores. Todo colaborador possui nome e um salário base privado. Um Professor recebe um
+// acréscimo de 20% sobre o salário base por dedicação exclusiva. Um Técnico Administrativo recebe
+// um valor fixo extra de auxílio-alimentação. O programa deve rodar em um laço solicitando o cadastro
+// dos funcionários do mês. Após coletar todos os dados, o software deve rodar a lista utilizando uma
+// estrutura de repetição, acionar o método de cálculo salarial específico de cada um e exibir o custo
+// total que a instituição terá naquele mês.
+
+
+export function poo_18 (){}
