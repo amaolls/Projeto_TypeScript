fba19d2a2b56e828918a7e8cfaa81e2a935072b5
amaolls <amddeoliveira@gmail.com>
2026-07-03 20:39:03 -0300

    Create poo_27.ts

---
+++ src/Exercicios/POO/poo_27.ts
@@ -0,0 +1,12 @@
+// Inventário Automatizado de Equipamentos de TI
+// Para organizar os laboratórios, crie um sistema de inventário. Todo equipamento possui número de
+// tombamento e descrição. Equipamentos do tipo Computador registram a quantidade de memória
+// RAM, enquanto equipamentos do tipo Roteador registram a quantidade de portas disponíveis. O
+// usuário deve alimentar um array inserindo os equipamentos que estão sendo catalogados no
+// laboratório atual. O sistema deve validar as entradas para não aceitar valores nulos ou inválidos. Ao
+// término do cadastro, o programa varre a lista inteira, disparando o método de auto-inspeção de cada
+// objeto para imprimir uma ficha técnica detalhada de cada item do almoxarifado.
+
+
+
+export function poo_27 (){}
