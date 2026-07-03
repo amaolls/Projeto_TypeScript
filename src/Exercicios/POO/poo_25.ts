f9b4f1403d1713417bd0a81eda776d6342ff158f
amaolls <amddeoliveira@gmail.com>
2026-07-03 20:39:08 -0300

    Create poo_25.ts

---
+++ src/Exercicios/POO/poo_25.ts
@@ -0,0 +1,13 @@
+// Aplicativo de Streaming e Assinaturas de Vídeo
+// Um provedor de internet quer lançar um serviço de streaming de vídeo. Cada assinatura possui o e-
+// mail do usuário e o valor do plano mensal. A Assinatura Padrão dá direito a 2 telas simultâneas. A
+// Assinatura Premium dá direito a 4 telas e inclui suporte a resolução 4K. O sistema deve pedir para o
+// atendente cadastrar novos clientes e selecionar seus planos correspondentes em um loop. Com os
+// dados salvos em uma lista de contratos, o programa deve permitir fazer uma busca pelo e-mail do
+// usuário e exibir o contrato detalhado formatado dinamicamente, revelando os benefícios e o preço
+// correto do plano escolhido por meio de polimorfismo.
+
+
+
+
+export function poo_25 (){}
