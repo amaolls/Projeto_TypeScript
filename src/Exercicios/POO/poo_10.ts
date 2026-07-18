1689ccf453ce71315fc39505ec1deb8a0c82e2d9
amaolls <amddeoliveira@gmail.com>
2026-07-18 16:20:26 -0300

    Update poo_10.ts

--- src/Exercicios/POO/poo_10.ts
+++ src/Exercicios/POO/poo_10.ts
@@ -8,4 +8,55 @@
 // devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer
 // momento.
 
-export function poo_10 (){}
+export function poo_10 (){
+    class Tamagushi {
+    nome: string
+    fome: number
+    saude: number
+    idade: number
+
+    constructor(nome: string, fome: number, saude: number, idade: number) {
+        this.nome = nome
+        this.fome = fome
+        this.saude = saude
+        this.idade = idade
+    }
+
+    // Métodos para alterar
+    alterarNome(novoNome: string): void { this.nome = novoNome; }
+    alterarFome(novaFome: number): void { this.fome = novaFome; }
+    alterarSaude(novaSaude: number): void { this.saude = novaSaude }
+    alterarIdade(novaIdade: number): void { this.idade = novaIdade }
+
+    // Métodos para retornar
+    retornarNome(): string { return this.nome }
+    retornarFome(): number { return this.fome }
+    retornarSaude(): number { return this.saude }
+    retornarIdade(): number { return this.idade }
+
+    // Campo calculado (Humor)
+    humor(): string {
+        let media = (this.fome + this.saude) / 2
+        if (media >= 8) return "Feliz ;)"
+        else if (media >= 5) return "Neutro :|"
+        else return "Triste :("
+    }
+}
+
+let nome = prompt("Nome do bichinho:")
+if(nome === null) return
+let fome = Number(prompt("Fome (0 a 10):"))
+let saude = Number(prompt("Saúde (0 a 10):"))
+let idade = Number(prompt("Idade:"))
+
+let bichinho = new Tamagushi(nome, fome, saude, idade)
+
+alert(
+    "Dados do Tamagushi:\n" +
+    "Nome: " + bichinho.retornarNome() + "\n" +
+    "Fome: " + bichinho.retornarFome() + "\n" +
+    "Saúde: " + bichinho.retornarSaude() + "\n" +
+    "Idade: " + bichinho.retornarIdade() + "\n" +
+    "Humor: " + bichinho.humor()
+)
+}
