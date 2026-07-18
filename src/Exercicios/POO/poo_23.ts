7447b39fdc219e5f9e2060b893681e29ddec285d
amaolls <amddeoliveira@gmail.com>
2026-07-18 16:21:11 -0300

    Update poo_23.ts

--- src/Exercicios/POO/poo_23.ts
+++ src/Exercicios/POO/poo_23.ts
@@ -10,4 +10,64 @@
 
 
 
-export function poo_23 (){}
+export function poo_23 (){
+class Produto {
+    codigo: number
+    nome: string
+    protected _precoCusto: number
+
+    constructor(codigo: number, nome: string, precoCusto: number) {
+        this.codigo = codigo
+        this.nome = nome
+        this._precoCusto = precoCusto
+    }
+
+    get precoCusto(): number { return this._precoCusto}
+
+    calcularPrecoFinal(): number {
+        return this._precoCusto
+    }
+}
+
+class Perecivel extends Produto {
+    dataValidade: string
+
+    constructor(codigo: number, nome: string, precoCusto: number, dataValidade: string) {
+        super(codigo, nome, precoCusto)
+        this.dataValidade = dataValidade
+    }
+
+    calcularPrecoFinal(): number {
+
+        return this._precoCusto * 0.70
+    }
+}
+
+class NaoPerecivel extends Produto {
+    constructor(codigo: number, nome: string, precoCusto: number) {
+        super(codigo, nome, precoCusto)
+    }
+
+    calcularPrecoFinal(): number {
+        return this._precoCusto
+    }
+}
+
+let cod = Number(prompt("Código:"))
+let nome = prompt("Nome:")
+if(nome === null) return
+let preco = Number(prompt("Preço de custo:"))
+let tipo = prompt("1 - Perecível\n2 - Não Perecível")
+
+let produto: Produto
+if (tipo === "1") {
+    let data = prompt("Data de validade:")
+    if(data === null) return
+    produto = new Perecivel(cod, nome, preco, data)
+} else {
+    produto = new NaoPerecivel(cod, nome, preco)
+}
+
+alert("Produto: " + produto.nome +
+      "\nPreço final: R$ " + produto.calcularPrecoFinal().toFixed(2))
+}
