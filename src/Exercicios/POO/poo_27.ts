31ac24340e8bff0e986f5a720b714f779537980d
amaolls <amddeoliveira@gmail.com>
2026-07-18 16:21:00 -0300

    Update poo_27.ts

--- src/Exercicios/POO/poo_27.ts
+++ src/Exercicios/POO/poo_27.ts
@@ -9,4 +9,62 @@
 
 
 
-export function poo_27 (){}
+export function poo_27 (){
+
+class Equipamento {
+    tombamento: string
+    descricao: string
+
+    constructor(tombamento: string, descricao: string) {
+        this.tombamento = tombamento
+        this.descricao = descricao
+    }
+
+    ficha(): string {
+        return "Tombamento: " + this.tombamento + "\nDescrição: " + this.descricao
+    }
+}
+
+class Computador extends Equipamento {
+    ram: number
+
+    constructor(tombamento: string, descricao: string, ram: number) {
+        super(tombamento, descricao)
+        this.ram = ram
+    }
+
+    ficha(): string {
+        return super.ficha() + "\nRAM: " + this.ram + " GB"
+    }
+}
+
+class Roteador extends Equipamento {
+    portas: number
+
+    constructor(tombamento: string, descricao: string, portas: number) {
+        super(tombamento, descricao)
+        this.portas = portas
+    }
+
+    ficha(): string {
+        return super.ficha() + "\nPortas: " + this.portas
+    }
+}
+
+let tomb = prompt("Número de tombamento:")
+if(tomb === null) return
+let desc = prompt("Descrição:")
+if(desc === null) return
+let tipo = prompt("1 - Computador\n2 - Roteador")
+
+let equip: Equipamento
+if (tipo === "1") {
+    let ram = Number(prompt("Quantidade de RAM (GB):"))
+    equip = new Computador(tomb, desc, ram)
+} else {
+    let portas = Number(prompt("Quantidade de portas:"))
+    equip = new Roteador(tomb, desc, portas)
+}
+
+alert("Ficha do equipamento:\n" + equip.ficha())
+}
